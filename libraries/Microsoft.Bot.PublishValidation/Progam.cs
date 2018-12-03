eae2a3f1ea06896a92bf7dffb83e6dda400f4115
Enzo Cano <enzo.cano@southworks.com>
2018-12-03 14:49:36 -0300

    Fix issue with dependencies

--- libraries/Microsoft.Bot.PublishValidation/Progam.cs
+++ libraries/Microsoft.Bot.PublishValidation/Progam.cs
@@ -1,12 +1,33 @@
-namespace Microsoft.Bot.PublishValidation
+﻿namespace Microsoft.Bot.PublishValidation
 {
     using System;
+    using Microsoft.Bot.Configuration;
 
     class Program
     {
-        static void Main(string[] args)
+        // Return codes
+        private const int ERROR = 2;
+        private const int OK = 0;
+
+        public static int Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Args: " + string.Join(", ", args));
+
+            var projectPath = args[0];
+
+            try
+            {
+                var bot = BotConfiguration.LoadFromFolder(projectPath);
+                Console.WriteLine($"{bot.Name} - {bot.Description}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ERROR;
+            }
+            
+            return OK;
         }
     }
 }
